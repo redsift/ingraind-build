e81861fbabb8c53537df3fd107b96ff90126e1ba
Peter Parkanyi <peter@redsift.io>
2018-07-30 14:59:49 +0100

    Initial commit

---
+++ kernels/4.16.12-1.el7.elrepo.x86_64/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP */
+#define UTS_MACHINE "x86_64"
+#define UTS_VERSION "#1 SMP Fri May 25 14:13:25 EDT 2018"
+#define LINUX_COMPILE_BY "mockbuild"
+#define LINUX_COMPILE_HOST "Build64R7"
+#define LINUX_COMPILER "gcc version 4.8.5 20150623 (Red Hat 4.8.5-28) (GCC)"
---
+++ kernels/4.16.12-1.el7.elrepo.x86_64/include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "4.16.12-1.el7.elrepo.x86_64"
